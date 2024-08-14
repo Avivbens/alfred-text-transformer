0ca1f4ea7e095de17c5f3ee7422852dd495b549d
Aviv Ben Shahar <avivbens87@gmail.com>
2024-08-15 02:18:50 +0300

    feat: support `Action Items` extractor

---
+++ src/main/action-items.ts
@@ -0,0 +1,39 @@
+import { FastAlfred } from 'fast-alfred'
+import { setTimeout } from 'node:timers/promises'
+import { ACTION_ITEMS_SYSTEM_PROMPT } from '@common/ai-prompts.constant'
+import { Variables } from '@common/variables'
+import { callOpenAI } from '@services/openai.service'
+
+;(async () => {
+    const alfredClient = new FastAlfred()
+
+    try {
+        const denounceTime = alfredClient.env.getEnv(Variables.DEBOUNCE_TIME, { defaultValue: 700, parser: Number })
+        const token: string | undefined = alfredClient.env.getEnv(Variables.OPEN_AI_TOKEN)
+
+        if (!token) {
+            throw new Error('OpenAI token is not defined')
+        }
+
+        /**
+         * Debounce time to wait for the user to finish typing
+         */
+        await setTimeout(denounceTime)
+
+        if (!alfredClient.input) {
+            throw new Error('Input is required')
+        }
+
+        const res = await callOpenAI(token, ACTION_ITEMS_SYSTEM_PROMPT, alfredClient.input)
+
+        const items = res.map((option) => ({
+            title: option,
+            subtitle: 'Action Items',
+            arg: option,
+        }))
+
+        alfredClient.output({ items })
+    } catch (error) {
+        alfredClient.error(error)
+    }
+})()
